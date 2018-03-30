f8e9924037329eaf60538e3061f5bfed58ee229a
Rick Cogley <rick.cogley@esolia.co.jp>
2018-03-30 16:59:24 +0900

    Set up bulma javascript

--- src/bulma/custom/_javascript/main.js
+++ src/bulma/custom/_javascript/main.js
@@ -1,3 +1,22 @@
+// Add files via prepend special codekit comment
+// at codekit - prepend "../../bower_components/bulma-accordion/dist/bulma-accordion.js";
+// @codekit-prepend "../../bower_components/bulma-badge/dist/bulma-badge.js";
+// @codekit-prepend "../../bower_components/bulma-calendar/dist/bulma-calendar.js";
+// at codekit-prepend "../../bower_components/bulma-carousel/dist/bulma-carousel.js";
+// @codekit-prepend "../../bower_components/bulma-checkradio/dist/bulma-checkradio.js";
+// @codekit-prepend "../../bower_components/bulma-divider/dist/bulma-divider.js";
+// @codekit-prepend "../../bower_components/bulma-iconpicker/dist/bulma-iconpicker.js";
+// @codekit-prepend "../../bower_components/bulma-pageloader/dist/bulma-pageloader.js";
+// at codekit - prepend "../../bower_components/bulma-pricingtable/dist/bulma-pricingtable.js";
+// @codekit-prepend "../../bower_components/bulma-quickview/dist/bulma-quickview.js";
+// @codekit-prepend "../../bower_components/bulma-ribbon/dist/bulma-ribbon.js";
+// @codekit-prepend "../../bower_components/bulma-slider/dist/bulma-slider.js";
+// @codekit-prepend "../../bower_components/bulma-steps/dist/bulma-steps.js";
+// @codekit-prepend "../../bower_components/bulma-switch/dist/bulma-switch.js";
+// @codekit-prepend "../../bower_components/bulma-tagsinput/dist/bulma-tagsinput.js";
+// @codekit-prepend "../../bower_components/bulma-timeline/dist/bulma-timeline.js";
+// @codekit-prepend "../../bower_components/bulma-tooltip/dist/bulma-tooltip.js";
+
 document.addEventListener('DOMContentLoaded', () => {
   console.log('Hello Bulma!');
 });
