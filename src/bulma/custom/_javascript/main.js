f1ccab5cb3df56c39521c3349cc525448938b6f0
Rick Cogley <rick.cogley@esolia.co.jp>
2018-05-06 08:11:38 +0900

    Comment out all unneeded bulma extensions (css and js)

--- src/bulma/custom/_javascript/main.js
+++ src/bulma/custom/_javascript/main.js
@@ -1,25 +1,25 @@
 // Add files via prepend special codekit comment
-// @codekit-prepend "../../bower_components/bulma-accordion/dist/bulma-accordion.js";
+// @ codekit -prepend "../../bower_components/bulma-accordion/dist/bulma-accordion.js";
 // at DEPRECATED codekit-prepend "../../bower_components/bulma-badge/dist/bulma-badge.js";
-// @codekit-prepend "../../bower_components/bulma-calendar/dist/bulma-calendar.js";
+// @ codekit -prepend "../../bower_components/bulma-calendar/dist/bulma-calendar.js";
 // @codekit-prepend "../../bower_components/bulma-carousel/dist/bulma-carousel.js";
 // at DEPRECATED codekit-prepend "../../bower_components/bulma-checkradio/dist/bulma-checkradio.js";
 // at DEPRECATED codekit-prepend "../../bower_components/bulma-divider/dist/bulma-divider.js";
-// @codekit-prepend "../../bower_components/bulma-iconpicker/dist/bulma-iconpicker.js";
+// @ codekit -prepend "../../bower_components/bulma-iconpicker/dist/bulma-iconpicker.js";
 // at DEPRECATED codekit-prepend "../../bower_components/bulma-pageloader/dist/bulma-pageloader.js";
-// at codekit - prepend "../../bower_components/bulma-pricingtable/dist/bulma-pricingtable.js";
+// @ codekit -prepend "../../bower_components/bulma-pricingtable/dist/bulma-pricingtable.js";
 // @codekit-prepend "../../bower_components/bulma-quickview/dist/bulma-quickview.js";
 // at DEPRECATED codekit-prepend "../../bower_components/bulma-ribbon/dist/bulma-ribbon.js";
-// @codekit-prepend "../../bower_components/bulma-slider/dist/bulma-slider.js";
+// @ codekit -prepend "../../bower_components/bulma-slider/dist/bulma-slider.js";
 // @codekit-prepend "../../bower_components/bulma-steps/dist/bulma-steps.js";
 // at DEPRECATED codekit-prepend "../../bower_components/bulma-switch/dist/bulma-switch.js";
-// @codekit-prepend "../../bower_components/bulma-tagsinput/dist/bulma-tagsinput.js";
+// @ codekit -prepend "../../bower_components/bulma-tagsinput/dist/bulma-tagsinput.js";
 // at DEPRECATED codekit-prepend "../../bower_components/bulma-timeline/dist/bulma-timeline.js";
 // at DEPRECATED codekit-prepend "../../bower_components/bulma-tooltip/dist/bulma-tooltip.js";
 
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('Hello Bulma!');
-});
+// document.addEventListener('DOMContentLoaded', () => {
+//   console.log('Hello Bulma!');
+// });
 
 // Mobile Hamburger Menu
 document.addEventListener('DOMContentLoaded', function () {
