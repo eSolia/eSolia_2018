f59f690ec16c3d0fa14506c72824f58da7f0fd3d
Rick Cogley <rick.cogley@esolia.co.jp>
2023-03-20 11:04:50 +0000

    Comment data deleters

--- serve.sh
+++ serve.sh
@@ -8,13 +8,13 @@ echo "Setup PROdb API Token in Env..."
 export PRODBTOKEN15331=$(cat $HOME/.ssh/tokens/prodb-01)
 
 echo "Get json files..."
-rm -i -rf $HOME/dev/eSolia_2018/data/success.json
+#rm -i -rf $HOME/dev/eSolia_2018/data/success.json
 #/usr/local/opt/curl/bin/curl --silent -k "https://pro.dbflex.net/secure/api/v2/15331/$(cat $HOME/.ssh/tokens/prodb-01)/Web%20Project/List%20All%20for%20JSON/select.json" | /usr/local/bin/jq '.' > $HOME/dev/eSolia_2018/data/success.json
-rm -i -rf $HOME/dev/eSolia_2018/data/info.json
+#rm -i -rf $HOME/dev/eSolia_2018/data/info.json
 # /usr/local/opt/curl/bin/curl --silent -k "https://pro.dbflex.net/secure/api/v2/15331/$(cat $HOME/.ssh/tokens/prodb-01)/Web%20Information/List%20All%20on%20Website/select.json" | /usr/local/bin/jq '.' > $HOME/dev/eSolia_2018/data/info.json
-rm -i -rf $HOME/dev/eSolia_2018/data/japancontact.json
+#rm -i -rf $HOME/dev/eSolia_2018/data/japancontact.json
 #/usr/local/opt/curl/bin/curl --silent -k "https://pro.dbflex.net/secure/api/v2/15331/$(cat $HOME/.ssh/tokens/prodb-01)/Web%20Japan%20Contact%20and%20App/API%20List%20All/select.json" | /usr/local/bin/jq '.' > $HOME/dev/eSolia_2018/data/japancontact.json
-rm -i -rf $HOME/dev/eSolia_2018/data/japanapp.json
+#rm -i -rf $HOME/dev/eSolia_2018/data/japanapp.json
 #/usr/local/opt/curl/bin/curl --silent -k "https://pro.dbflex.net/secure/api/v2/15331/$(cat $HOME/.ssh/tokens/prodb-01)/Web%20Japan%20Contact%20and%20App/API%20List%20All%20Apps/select.json" | /usr/local/bin/jq '.' > $HOME/dev/eSolia_2018/data/japanapp.json
 rm -i -rf $HOME/dev/eSolia_2018/static/eSolia-Japan-Emergency-Contacts.en.csv
 /usr/local/opt/curl/bin/curl --silent -k "https://pro.dbflex.net/secure/api/v2/15331/$(cat $HOME/.ssh/tokens/prodb-01)/Web%20Japan%20Contact%20and%20App/API%20List%20for%20CSV/select.csv" -o $HOME/dev/eSolia_2018/static/eSolia-Japan-Emergency-Contacts.en.csv
